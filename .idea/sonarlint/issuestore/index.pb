d47bccb035f34c868d8545c89e6d9628d442d2a4
Smit <susmit025@gmail.com>
2022-11-14 13:47:57 +0530

    DiffUtil with Recycler View

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,9 @@
+
+@
+local.properties,0/7/0712df971a99ac4d2fccb8e0fb19f377f3374cca
+X
+(gradle/wrapper/gradle-wrapper.properties,f/b/fbe448ebfc3eb2d4e308f6b8b043666f5b57235e
+P
+ app/src/main/AndroidManifest.xml,8/c/8c55c3ccc257e5907959013f99656e4c8ec3903e
+p
+@app/src/main/java/com/androiddevs/mvvmnewsapp/UI/NewsActivity.kt,5/2/52234b4072aba1d9f798f84dd7545ad27e827731
---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/adapter/NewsAdapter.kt
@@ -0,0 +1,68 @@
+package com.androiddevs.mvvmnewsapp.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.androiddevs.mvvmnewsapp.R
+import com.androiddevs.mvvmnewsapp.db.models.Article
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.item_article_preview.view.*
+
+class NewsAdapter : RecyclerView.Adapter<NewsAdapter.ArticleViewHolder>() {
+
+    inner class ArticleViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): NewsAdapter.ArticleViewHolder {
+        return ArticleViewHolder(
+            LayoutInflater.from(parent.context)
+                .inflate(R.layout.item_article_preview, parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: NewsAdapter.ArticleViewHolder, position: Int) {
+        val article = differ.currentList[position]
+        holder.itemView.apply {
+            Glide.with(this).load(article.urlToImage).into(ivArticleImage)
+            tvSource.text = article.source.name
+            tvTitle.text = article.title
+            tvDescription.text = article.description
+            tvPublishedAt.text = article.publishedAt
+            setOnClickListener {
+                onItemClickListener?.let {
+                    it(article)
+                }
+            }
+        }
+    }
+
+    var onItemClickListener: ((Article) -> Unit)? = null
+
+    fun setOnClickListener(listener: (Article) -> Unit) {
+        onItemClickListener = listener
+    }
+
+    override fun getItemCount(): Int {
+        return differ.currentList.size
+    }
+
+    private val differCallBack = object : DiffUtil.ItemCallback<Article>() {
+        override fun areItemsTheSame(oldItem: Article, newItem: Article): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: Article, newItem: Article): Boolean {
+            return oldItem == newItem
+        }
+
+    }
+
+    val differ = AsyncListDiffer(this, differCallBack)
+}
