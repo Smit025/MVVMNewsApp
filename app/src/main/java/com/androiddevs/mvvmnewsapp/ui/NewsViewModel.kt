0413993517104088c2230e9e0902be1f8afff1ed
Smit <susmit025@gmail.com>
2022-11-15 01:03:34 +0530

    Added Search View and its Implementation

--- app/src/main/java/com/androiddevs/mvvmnewsapp/ui/NewsViewModel.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/NewsViewModel.kt
@@ -3,8 +3,6 @@ package com.androiddevs.mvvmnewsapp.ui
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.androiddevs.mvvmnewsapp.api.RetrofitInstance
-import com.androiddevs.mvvmnewsapp.db.models.Article
 import com.androiddevs.mvvmnewsapp.db.models.NewsResponse
 import com.androiddevs.mvvmnewsapp.repository.NewsRepository
 import com.androiddevs.mvvmnewsapp.util.Resource
@@ -15,7 +13,9 @@ import retrofit2.Response
 class NewsViewModel(val newsRepository: NewsRepository) : ViewModel() {
 
     val breakingNews: MutableLiveData<Resource<NewsResponse>> = MutableLiveData()
-    private val breakingNewPage = 1
+    private val breakingNewsPage = 1
+    val searchNews: MutableLiveData<Resource<NewsResponse>> = MutableLiveData()
+    private val searchNewsPage = 1
 
     init {
         getBreakingNews("in")
@@ -23,8 +23,14 @@ class NewsViewModel(val newsRepository: NewsRepository) : ViewModel() {
 
     private fun getBreakingNews(countryCode: String) = viewModelScope.launch {
         breakingNews.postValue(Resource.Loading())
-        val response = newsRepository.getBreakingNews(countryCode, breakingNewPage)
+        val response = newsRepository.getBreakingNews(countryCode, breakingNewsPage)
         breakingNews.postValue(handlingBreakingNewsResponse(response))
+    }
+
+    fun searchNews(searchQuery: String) = viewModelScope.launch {
+        searchNews.postValue(Resource.Loading())
+        val response = newsRepository.searchForNews(searchQuery, searchNewsPage)
+        searchNews.postValue(handlingSearchNewsResponse(response))
     }
 
     private fun handlingBreakingNewsResponse(response: Response<NewsResponse>): Resource<NewsResponse> {
@@ -32,6 +38,16 @@ class NewsViewModel(val newsRepository: NewsRepository) : ViewModel() {
         if (response.isSuccessful) {
             response.body()?.let {
                 return Resource.Success(it)
+            }
+        }
+        return Resource.Error(response.message())
+    }
+
+    private fun handlingSearchNewsResponse(response: Response<NewsResponse>): Resource<NewsResponse> {
+
+        if (response.isSuccessful) {
+            response.body()?.let {searchResponse ->
+                return Resource.Success(searchResponse)
             }
         }
         return Resource.Error(response.message())
